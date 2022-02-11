058f7c3da878a7f7f7fcdbdcd5166fe7c8df7e75
Victor <vict02r05@gmail.com>
2022-02-11 22:37:43 +0200

    add json-server after adding an npm manager

--- js/script.js
+++ js/script.js
@@ -292,4 +292,8 @@ window.addEventListener('DOMContentLoaded', () => {
       closeModal();
     }, 4000);
   }
+
+  fetch('http://localhost:3000/menu')
+    .then(data => data.json())
+    .then(res => console.log(res));
 });
