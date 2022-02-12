6ea1713078ecf4d48e1af101118f45b91d38e623
Victor <vict02r05@gmail.com>
2022-02-12 17:33:20 +0200

    Delete disposable markup function
    Change independence  markup function on templating in class method as instance of the class. Use destructuring in it.

--- js/script.js
+++ js/script.js
@@ -193,29 +193,18 @@ window.addEventListener('DOMContentLoaded', () => {
     return await res.json();
   };
 
-  getResource('http://localhost:3000/menu').then(data => createCard(data));
-
-  function createCard(data) {
+  getResource('http://localhost:3000/menu').then(data => {
     data.forEach(({ img, altimg, title, descr, price }) => {
-      const element = document.createElement('div');
-      element.classList.add('menu__item');
-      element.innerHTML = `
-      <img src=${img} alt=${altimg} />
-      <h3 class="menu__item-subtitle">${title}</h3>
-      <div class="menu__item-descr">
-        ${descr}
-      </div>
-      <div class="menu__item-divider"></div>
-      <div class="menu__item-price">
-        <div class="menu__item-cost">Цена:</div>
-        <div class="menu__item-total"><span>${(price * 28.2).toFixed(
-          0
-        )}</span> грн/день</div>
-      </div>`;
-
-      document.querySelector('.menu .container').append(element);
+      new MenuCard(
+        img,
+        altimg,
+        title,
+        descr,
+        price,
+        '.menu .container'
+      ).render();
     });
-  }
+  });
 
   // Forms
 
